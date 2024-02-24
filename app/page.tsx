dbd45782efda61be1b5e12ce6abc1b52f64a7c61
BizarreAvatar <55567726+andrewtam6@users.noreply.github.com>
2024-02-23 18:35:56 -0600

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { WavyBackground } from "@/components/ui/wavy-background";
+
 export default function Home() {
   return (
     <>
-      Hello world!
+      <WavyBackground>
+        <p className="text-white text-8xl text-center font-bold p-4">Andrew Tamayo</p>
+        <p className="text-white text-s text-center font-bold ">Computer Science Student @ University of Tennessee &mdash; Knoxville</p>
+      </WavyBackground>
     </>
   );
 }
